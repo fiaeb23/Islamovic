fff77022740b571eed176da0df2445511bb915cc
fiaeb23 <fiae.b.23@gmail.com>
2023-04-11 13:37:39 +0200

    ''

---
+++ Python/Aufgaben - Ubung/10_nutzerprofil.py
@@ -0,0 +1,25 @@
+"""
+Erstelle ein CLI-Tool mit dem sich ein Nutzerprofil darstellen lässt
+
+Hole zunächst folgende Informationen vom Nutzer:
+1) Name
+   - Ein Name muss aus mind. 3 Zeichen bestehen
+   - Ein Name darf keine Zahlen enthalten
+2) Geburtsjahr
+   - Es muss ein valides und "realistisches" Geburtsjahr sein
+   - Das Mindestalter zur Nutzung des Programms ist 18
+3) Wohnort
+   - Ein Wohnort muss aus mind. 5 Zeichen bestehen
+   - Ein Wohnort darf keine Zahlen enthalten
+   
+Anschließend kann der Nutzer über ein Menü:
+- Seinen Namen verändern
+  (beachte die Namens-Regeln!)
+- Sein Geburtsjahr verändern
+  (beachte die Geburtsjahr-Regeln!)
+- Seinen Wohnort verändern
+  (beachte die Wohnort-Regeln!)
+- Programm beenden
+
+Tipp: Verwende die len()-Funktion
+"""
