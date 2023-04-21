0443344aa02e9f3cb154c927a5a59fdd018e0e38
fiaeb23 <fiae.b.23@gmail.com>
2023-04-21 11:12:15 +0200

    Listen

---
+++ Python/Aufgaben - Ubung/16_kino.py
@@ -0,0 +1,20 @@
+
+kino_reihe = ["X", None, "X", None, None]
+# "X" -> Platz belegt
+# None -> Platz frei
+
+""" Entwickle ein Kino-System
+
+Der Nutzer hat folgende Optionen:
+1) Den ersten freien Einzelplatz ermitteln
+   - Gebe die Position des ersten freien Platzes aus
+2) Den ersten freien Doppelplatz ermitteln
+   - Gebe die Position des ersten freien Doppelplatzes aus
+3) Kino verlassen
+"""
+
+while True:
+    print("Du hast folgende Optionen:")
+    .
+    .
+    .
